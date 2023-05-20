dd7493e6cc2ce629646d634206ad8b486602af06
vmore <themoonissilent@gmail.com>
2023-05-20 20:05:14 +0300

    add remaps for nvim

--- .config/nvim/lua/vmore/remaps.lua
+++ .config/nvim/lua/vmore/remaps.lua
@@ -1,3 +1,31 @@
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+-- let cursor stay at its plays when appending lines
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- let cursor stay in the middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- cursor stays in the middle when searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- delete highlighted in to the void buffer so current buffer stays intact
+vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- yank into system register
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "Q", "<nop>")
+
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
